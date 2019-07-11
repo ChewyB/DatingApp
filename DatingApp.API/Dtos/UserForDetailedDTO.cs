51406642064f6794106e42969595e78a2e0e5e72
ChewyB <Jb515764>
2019-07-10 19:20:08 -0700

    Added a PhotosDetailedDTO since we had issues with Photo returning unwanted User data

--- DatingApp.API/Dtos/UserForDetailedDTO.cs
+++ DatingApp.API/Dtos/UserForDetailedDTO.cs
@@ -11,7 +11,7 @@ namespace DatingApp.API.Dtos
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
         public string Gender { get; set; }     
-        public int DateAgeOfBirth { get; set; }
+        public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
@@ -21,6 +21,6 @@ namespace DatingApp.API.Dtos
         public string City { get; set; }
         public string Country { get; set; }
         public string PhotoUrl { get; set; }
-        public ICollection<Photo> Photos { get; set; }
+        public ICollection<PhotosForDetailedDTO> Photos { get; set; }
     }
 }
